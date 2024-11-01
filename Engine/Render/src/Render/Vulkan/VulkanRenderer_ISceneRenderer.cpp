37fef21e3d754e48e3f57214a4591b3b96c0962c
amasson <arthur.masson.3896@gmail.com>
2024-11-01 12:52:09 +0100

    fix clang

--- Engine/Render/src/Render/Vulkan/VulkanRenderer_ISceneRenderer.cpp
+++ Engine/Render/src/Render/Vulkan/VulkanRenderer_ISceneRenderer.cpp
@@ -93,7 +93,9 @@ void VulkanRenderer::_recordCommandBuffer(VkCommandBuffer commandBuffer, ImageCo
 	}
 
 	std::array<VkClearValue, 2> clearValues = {};
-	clearValues[0].color = {0.0f, 0.0f, 0.0f, 1.0f};
+	clearValues[0].color = {
+		{0.0f, 0.0f, 0.0f, 1.0f}
+	};
 	clearValues[1].depthStencil = {1.0f, 0};
 
 	VkRenderPassBeginInfo renderPassInfo = {};
