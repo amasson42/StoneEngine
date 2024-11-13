f5e96e27fd0c91af00f36f7fcb7fddcc61ff10de
amasson <arthur.masson.3896@gmail.com>
2024-11-13 08:43:25 +0100

    Separate texture header and implementation

--- Engine/Render/src/Render/OpenGL/Renderable/Texture.hpp
+++ Engine/Render/src/Render/OpenGL/Renderable/Texture.hpp
@@ -11,40 +11,17 @@
 
 namespace Stone::Render::OpenGL {
 
-GLuint convert(Core::Image::Channel channel) {
-	switch (channel) {
-	case Core::Image::Channel::GREY: return GL_RED;
-	case Core::Image::Channel::DUAL: return GL_RG;
-	case Core::Image::Channel::RGB: return GL_RGB;
-	case Core::Image::Channel::RGBA: return GL_RGBA;
-	default: return GL_RGBA;
-	}
-}
+GLuint convert(Core::Image::Channel channel);
 
-GLuint convert(Scene::TextureFilter filter, bool mipmap) {
-	switch (filter) {
-	case Scene::TextureFilter::Nearest: return mipmap ? GL_NEAREST_MIPMAP_NEAREST : GL_NEAREST;
-	case Scene::TextureFilter::Linear: return mipmap ? GL_LINEAR_MIPMAP_LINEAR : GL_LINEAR;
-	case Scene::TextureFilter::Cubic: return mipmap ? GL_LINEAR_MIPMAP_LINEAR : GL_LINEAR;
-	default: return mipmap ? GL_LINEAR_MIPMAP_LINEAR : GL_LINEAR;
-	}
-}
+GLuint convert(Scene::TextureFilter filter, bool mipmap);
 
-GLuint convert(Scene::TextureWrap wrap) {
-	switch (wrap) {
-	case Scene::TextureWrap::Repeat: return GL_REPEAT;
-	case Scene::TextureWrap::MirroredRepeat: return GL_MIRRORED_REPEAT;
-	case Scene::TextureWrap::ClampToEdge: return GL_CLAMP_TO_EDGE;
-	case Scene::TextureWrap::ClampToBorder: return GL_CLAMP_TO_BORDER;
-	default: return GL_REPEAT;
-	}
-}
+GLuint convert(Scene::TextureWrap wrap);
 
 
 class Texture : public Scene::IRendererObject {
 public:
-	Texture(Scene::Texture &texture, const std::shared_ptr<OpenGLRenderer> &renderer)
-		: _texture(texture), _renderer(renderer) {
+	Texture(Scene::Texture &texture, const std::shared_ptr<OpenGLRenderer> &renderer) : _gl_texture(0) {
+		(void)renderer;
 
 		std::shared_ptr<Core::Image::ImageSource> imageSource = texture.getImage();
 		if (imageSource == nullptr) {
@@ -55,11 +32,11 @@ public:
 		std::shared_ptr<Core::Image::ImageData> imageData = imageSource->getLoadedImage();
 
 		GLuint format = convert(imageData->getChannels());
-		glGenTextures(1, &_buffer);
-		if (_buffer == 0) {
+		glGenTextures(1, &_gl_texture);
+		if (_gl_texture == 0) {
 			std::runtime_error("Failed to create texture buffer.");
 		}
-		glBindTexture(GL_TEXTURE_2D, _buffer);
+		glBindTexture(GL_TEXTURE_2D, _gl_texture);
 		glTexImage2D(GL_TEXTURE_2D, 0, format, imageData->getSize().x, imageData->getSize().y, 0, format,
 					 GL_UNSIGNED_BYTE, imageData->getData());
 
@@ -72,8 +49,8 @@ public:
 	}
 
 	~Texture() override {
-		if (_buffer != 0) {
-			glDeleteTextures(1, &_buffer);
+		if (_gl_texture != 0) {
+			glDeleteTextures(1, &_gl_texture);
 		}
 	}
 
@@ -81,15 +58,12 @@ public:
 		(void)context;
 	}
 
-	GLuint getBuffer() const {
-		return _buffer;
+	GLuint getGlTexture() const {
+		return _gl_texture;
 	}
 
 private:
-	Scene::Texture &_texture;
-	std::weak_ptr<OpenGLRenderer> _renderer;
-
-	GLuint _buffer = 0;
+	GLuint _gl_texture = 0;
 };
 
 } // namespace Stone::Render::OpenGL
