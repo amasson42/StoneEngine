77d891687789a38be38b43a6220549647d75945b
amasson <giantwow3896@gmail.com>
2024-11-14 00:37:51 +0100

    opengl mesh node pipeline

--- Engine/Render/src/Render/OpenGL/Renderable/MeshNode.hpp
+++ Engine/Render/src/Render/OpenGL/Renderable/MeshNode.hpp
@@ -2,65 +2,26 @@
 
 #pragma once
 
-#include "../OpenGLResources.hpp"
-#include "Material.hpp"
-#include "Mesh.hpp"
-#include "Render/OpenGL/OpenGLRenderer.hpp"
 #include "Scene/Node/MeshNode.hpp"
-#include "Scene/Renderable/Mesh.hpp"
-
-#include <GL/glew.h>
 
 namespace Stone::Render::OpenGL {
 
+class Material;
+class OpenGLRenderer;
+class ShaderCollection;
+
 class MeshNode : public Scene::IRendererObject {
 public:
-	MeshNode(Scene::MeshNode &meshNode, const std::shared_ptr<OpenGLRenderer> &renderer)
-		: _meshNode(meshNode), _renderer(renderer) {
-
-		std::shared_ptr<Scene::Material> usedMaterial =
-			meshNode.getMaterial() ? meshNode.getMaterial()
-			: meshNode.getMesh() != nullptr && meshNode.getMesh()->getDefaultMaterial() != nullptr
-				? meshNode.getMesh()->getDefaultMaterial()
-				: nullptr;
-	}
+	MeshNode(Scene::MeshNode &meshNode, const std::shared_ptr<OpenGLRenderer> &renderer);
 
 	~MeshNode() override = default;
 
-	void render(Scene::RenderContext &context) override {
-		auto mesh = _meshNode.getMesh();
-		if (mesh == nullptr)
-			return;
-
-		auto rendererMesh = mesh->getRendererObject<RendererMesh>();
-		if (rendererMesh == nullptr) {
-			return;
-		}
-		const VRAMMesh &vramMesh = rendererMesh->getVRAMMesh();
-
-		std::shared_ptr<Scene::Material> sceneMaterial = _meshNode.getMaterial()	  ? _meshNode.getMaterial()
-														 : mesh->getDefaultMaterial() ? mesh->getDefaultMaterial()
-																					  : nullptr;
-
-		assert(sceneMaterial != nullptr);
-		assert(sceneMaterial->isDirty() == false);
-
-		std::shared_ptr<Material> material = sceneMaterial->getRendererObject<Material>();
-
-		material->render(context);
-
-		glEnable(GL_DEPTH_TEST);
-		glDepthFunc(GL_LESS);
-		glEnable(GL_CULL_FACE);
-		glCullFace(GL_BACK);
-
-		glBindVertexArray(vramMesh.elementsBuffer);
-		glDrawElements(GL_TRIANGLES, vramMesh.numIndices, GL_UNSIGNED_INT, 0);
-	}
+	void render(Scene::RenderContext &context) override;
 
 private:
 	Scene::MeshNode &_meshNode;
-	std::weak_ptr<OpenGLRenderer> _renderer;
+	Material *_material;
+	ShaderCollection *_shaderCollection;
 };
 
 } // namespace Stone::Render::OpenGL
