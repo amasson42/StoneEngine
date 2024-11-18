eec16766031a4352eb8fcc6fb622915b6425be35
amasson <giantwow3896@gmail.com>
2024-11-18 17:01:12 +0100

    string to base 64 utilities

--- Engine/Utils/src/Utils/StringExt.cpp
+++ Engine/Utils/src/Utils/StringExt.cpp
@@ -28,10 +28,10 @@ bool string_contains_non_printable(const std::string &str) {
 	});
 }
 
-std::string int_to_hex(unsigned char c) {
+std::string int_to_hex(unsigned char c, int width) {
 	std::stringstream ss;
 	ss << std::hex << std::uppercase;
-	ss << std::setw(4) << std::setfill('0') << static_cast<int>(c);
+	ss << std::setw(width) << std::setfill('0') << static_cast<int>(c);
 	return ss.str();
 }
 
@@ -56,4 +56,53 @@ std::string escape_string(const std::string &str) {
 		}
 	}
 	return result;
+}
+
+const static std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+void bytes_to_base64(const u_int8_t *bytes, std::size_t size, std::string &out) {
+	out.reserve(out.size() + ((size + 2) / 3) * 4);
+
+	for (std::size_t i = 0; i < size; i += 3) {
+		u_int8_t b0, b1, b2;
+		b0 = static_cast<u_int8_t>(bytes[i]);
+		b1 = i + 1 < size ? static_cast<u_int8_t>(bytes[i + 1]) : 0;
+		b2 = i + 2 < size ? static_cast<u_int8_t>(bytes[i + 2]) : 0;
+
+		u_int8_t c0, c1, c2, c3;
+		c0 = b0 >> 2;
+		c1 = ((b0 & 0x03) << 4) | (b1 >> 4);
+		c2 = ((b1 & 0x0f) << 2) | (b2 >> 6);
+		c3 = b2 & 0x3f;
+
+		out += base64_chars[c0];
+		out += base64_chars[c1];
+		out += i + 1 < size ? base64_chars[c2] : '=';
+		out += i + 2 < size ? base64_chars[c3] : '=';
+	}
+}
+
+void base64_to_bytes(const std::string &base64, std::vector<u_int8_t> &out) {
+	out.reserve(out.size() + (base64.size() / 4) * 3);
+
+	for (size_t i = 0; i < base64.size(); i += 4) {
+		u_int8_t c0, c1, c2, c3;
+		c0 = base64_chars.find(base64[i]);
+		c1 = base64_chars.find(base64[i + 1]);
+		c2 = base64[i + 2] == '=' ? 0 : base64_chars.find(base64[i + 2]);
+		c3 = base64[i + 3] == '=' ? 0 : base64_chars.find(base64[i + 3]);
+
+		u_int8_t b0, b1, b2;
+		b0 = (c0 << 2) | (c1 >> 4);
+		b1 = ((c1 & 0x0f) << 4) | (c2 >> 2);
+		b2 = ((c2 & 0x03) << 6) | c3;
+
+		out.push_back(b0);
+		if (base64[i + 2] != '=') {
+			out.push_back(b1);
+		}
+		if (base64[i + 3] != '=') {
+			out.push_back(b2);
+		}
+	}
 }
